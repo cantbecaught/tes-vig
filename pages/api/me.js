9dd27f23e9a84b8b8236d257c1fd5c377958fb83
cantbecaught <rvlzpg@gmail.com>
2025-05-24 06:29:10 +0700

    Update me.js

--- pages/api/me.js
+++ pages/api/me.js
@@ -1,6 +1,6 @@
-import dbConnect from '@/lib/dbConnect';
-import User from '@/models/User';
-import { verifyToken } from '@/lib/auth';
+import dbConnect from '../../lib/dbConnect';
+import User from '../../models/User';
+import { verifyToken } from '../../lib/auth';
 
 export default async function handler(req, res) {
   await dbConnect();
