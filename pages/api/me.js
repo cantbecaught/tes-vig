b1d40ef23cb8b6ef08963ff7c6a944d4150280bf
cantbecaught <rvlzpg@gmail.com>
2025-05-24 06:18:41 +0700

    Update me.js

--- pages/api/me.js
+++ pages/api/me.js
@@ -1,10 +1,16 @@
-import { verifyToken } from '../../lib/auth';
-import prisma from '../../lib/prisma';
+import dbConnect from '@/lib/dbConnect';
+import User from '@/models/User';
+import { verifyToken } from '@/lib/auth';
 
 export default async function handler(req, res) {
+  await dbConnect();
+
   const token = req.cookies.token;
   const data = verifyToken(token);
   if (!data) return res.status(401).json({});
-  const user = await prisma.user.findUnique({ where: { id: data.id } });
-  res.status(200).json({ id: user.id, username: user.username });
+
+  const user = await User.findById(data.id).select('_id username');
+  if (!user) return res.status(404).json({ message: 'User not found' });
+
+  res.status(200).json({ id: user._id, username: user.username });
 }
