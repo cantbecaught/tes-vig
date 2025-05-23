36bacc3b30591ae6372d3e657e3d6a6abdb6bb6d
cantbecaught <rvlzpg@gmail.com>
2025-05-24 06:47:48 +0700

    Update register.js

--- pages/auth/register.js
+++ pages/auth/register.js
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { useRouter } from 'next/router';
+import Link from 'next/link';
 
 export default function Register() {
   const [form, setForm] = useState({ username: '', password: '' });
